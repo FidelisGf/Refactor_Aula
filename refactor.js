f95bf4ceda2e565839d2525ee6fe7be4a4f16b88
Gabriel Fidelis Pereira <jpinformaticafoz@gmail.com>
2022-10-18 21:01:05 -0300

    `feito`

--- refactor.js
+++ refactor.js
@@ -1,27 +1,16 @@
 function returnRandomNumber() {
-  return Math.random();
+    let num  = Math.random();
+    return Math.round(num * 100)
 }
-
 function sumToRandomNumber(num) {
-  const numberToSum = returnRandomNumber();
-
-  return `Seu número é ${Math.round(numberToSum * 100) + num}!`;
+  return `Seu número é ${returnRandomNumber() + num}!`;
 }
-
 function subtractRandomNumber(num) {
-  const numberToSubtract = returnRandomNumber();
-
-  return `Seu número é ${Math.round(numberToSubtract * 100) - num}!`;
+  return `Seu número é ${returnRandomNumber() - num}!`;
 }
-
 function multiplyToRandomNumber(num) {
-  const numberToMultiply = returnRandomNumber();
-
-  return `Seu número é ${Math.round(numberToMultiply * 100) * num}!`;
+  return `Seu número é ${returnRandomNumber() * num}!`;
 }
-
 function divideRandomNumber(num) {
-  const numberToDivide = returnRandomNumber();
-
-  return `Seu número é ${Math.round(numberToDivide * 100) / num}!`;
+  return `Seu número é ${returnRandomNumber() / num}!`;
 }
